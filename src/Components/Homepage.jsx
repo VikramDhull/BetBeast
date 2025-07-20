fea0746cf022d5ec963f43216696e674c15ca2b8
Vikram Dhull <imvikramdhull@gmail.com>
2025-07-20 14:11:42 +0530

    add logic for COLOR TYPE SIZE

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -6,23 +6,33 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Homepage = () => {
   let red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+  let black = [
+    2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35,
+  ];
   let even = [
     2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36,
   ];
+  let odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35];
   let small = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+  let big = [
+    19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+  ];
 
   const submitCount = useRef(0);
+  const [dummy, setDummy] = useState(0);
   const [buttonDisable, setButtonDisable] = useState(false);
   const [initialBet, setInitialBet] = useState("");
   const [previousNumber, setPreviousNumber] = useState("");
   const [submittedPreviousNumber, setSubmittedPreviousNumber] = useState("");
+  const [secondPreviousNumber, setSecondPreviousNumber] = useState("");
   const [startingAmount, setStartingAmount] = useState("");
   const [displayName, setDisplayName] = useState({
     color: "",
     type: "",
     size: "",
   });
-  let temp;
+  const [displayCalculation2, setDisplayCalculation2] = useState("");
+  const [displayCalculation3, setDisplayCalculation3] = useState("");
 
   const isValidNumber = (val) => {
     return /^\d+$/.test(val); // only digits, no decimal, no negatives
@@ -38,10 +48,11 @@ const Homepage = () => {
     if (startingAmount !== "" && submittedPreviousNumber !== "") {
       handleDisplayCalculation();
     }
-  }, [startingAmount, submittedPreviousNumber]);
+  }, [startingAmount, submittedPreviousNumber, dummy]);
 
   let handleFormSubmit = (event) => {
     event.preventDefault();
+    setDummy((d) => d + 1);
     if (
       !isValidNumber(previousNumber) ||
       parseInt(previousNumber) < 0 ||
@@ -61,8 +72,8 @@ const Homepage = () => {
         setButtonDisable(true);
       }
     }
+    setSecondPreviousNumber(parseInt(submittedPreviousNumber));
     setSubmittedPreviousNumber(parseInt(previousNumber));
-
     setPreviousNumber("");
   };
 
@@ -79,9 +90,50 @@ const Homepage = () => {
     }
   };
 
+  // console.log(`starting amount : ${startingAmount}`);
+  // console.log(`previous number : ${submittedPreviousNumber}`);
+  // console.log(`second previus number ${secondPreviousNumber}`);
   const handleDisplayCalculation = () => {
-    console.log(`starting amount : ${startingAmount}`);
-    console.log(`previous number : ${submittedPreviousNumber}`);
+    if (submitCount.current == 1) {
+      setDisplayCalculation2(startingAmount);
+      setDisplayCalculation3(startingAmount);
+      submitCount.current++;
+      return;
+    }
+    if (submittedPreviousNumber == 0) {
+      setDisplayCalculation2((prev) => prev * 2);
+      setDisplayCalculation3((prev) => prev * 3);
+      return;
+    }
+    let colorVal = displayCalculation2;
+    let typeVal = displayCalculation2;
+    let sizeVal = displayCalculation2;
+    colorVal =
+      (red.includes(submittedPreviousNumber) &&
+        red.includes(secondPreviousNumber)) ||
+      (black.includes(submittedPreviousNumber) &&
+        black.includes(secondPreviousNumber))
+        ? startingAmount
+        : colorVal * 2;
+
+    typeVal =
+      (even.includes(submittedPreviousNumber) &&
+        even.includes(secondPreviousNumber)) ||
+      (odd.includes(submittedPreviousNumber) &&
+        odd.includes(secondPreviousNumber))
+        ? startingAmount
+        : typeVal * 2;
+
+    sizeVal =
+      (small.includes(submittedPreviousNumber) &&
+        small.includes(secondPreviousNumber)) ||
+      (big.includes(submittedPreviousNumber) &&
+        big.includes(secondPreviousNumber))
+        ? startingAmount
+        : sizeVal * 2;
+
+    let result2 = Math.ceil((colorVal + typeVal + sizeVal) / 3 / 10) * 10;
+    setDisplayCalculation2(result2);
   };
 
   return (
@@ -92,9 +144,10 @@ const Homepage = () => {
       <div className="homepage-main">
         <div className="display">
           <h2>
-            {displayName.color} / {displayName.type} / {displayName.size} : ₹11
+            {displayName.color} / {displayName.type} / {displayName.size} : ₹
+            {displayCalculation2}
           </h2>
-          <h2>2 QUADRANT / 2 LINE : ₹11</h2>
+          <h2>2 QUADRANT / 2 LINE : ₹{displayCalculation3}</h2>
         </div>
         <div className="form">
           <form onSubmit={handleFormSubmit}>
