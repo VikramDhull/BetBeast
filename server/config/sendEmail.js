bbc1f1b75b9fedd22ea9e7b53ada1961029c5003
Vikram Dhull <imvikramdhull@gmail.com>
2025-10-14 13:34:20 +0530

    checking nodemailer

--- server/config/sendEmail.js
+++ server/config/sendEmail.js
@@ -1,22 +1,54 @@
 import nodemailer from "nodemailer";
 
+// const sendEmail = async (to, subject, text) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.MY_EMAIL,
+//       pass: process.env.MY_PASS,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: process.env.MY_EMAIL,
+//     to,
+//     subject,
+//     text,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
 const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.MY_EMAIL,
-      pass: process.env.MY_PASS,
-    },
-  });
+  try {
+    console.log("Preparing to send email...");
 
-  const mailOptions = {
-    from: process.env.MY_EMAIL,
-    to,
-    subject,
-    text,
-  };
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.MY_EMAIL,
+        pass: process.env.MY_PASS,
+      },
+    });
 
-  await transporter.sendMail(mailOptions);
+    console.log("Transporter created.");
+
+    const mailOptions = {
+      from: process.env.MY_EMAIL,
+      to,
+      subject,
+      text,
+    };
+
+    console.log("Mail options set. Sending email...");
+
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log("Email sent:", info.messageId);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
 };
 
 export default sendEmail;
